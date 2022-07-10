43985554a24c78ebb345c6e7877009e726d583ac
Prathamesh Patil <prathameshp.2001@gmail.com>
2022-07-10 22:47:52 +0530

    first commit

---
+++ jvs.js
@@ -0,0 +1,14 @@
+  <html>
+      <head>
+           <title> heading levels </title>
+		</head>
+  <body bgcolor =skyblue>
+    <h1> heading level 1 text is largest in size </h1>
+	<h2> heading level 2 </h2>
+	 <h3> headinglevel  3 </3>
+	  <h4> heading level 4 </4>
+	   <h5> heading level 5 </5>
+	    <h6> heading level text is smallest in size 6 </h6>
+	</body>
+	</html>.
+		
